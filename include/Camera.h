6a947049a45b2ce5b9eb1e628ffc0f3c2bb4cd4e
Tag Ashby <tgashby@calpoly.edu>
2011-08-10 15:25:27 -0700

    Camera pseudocoded

--- include/Camera.h
+++ include/Camera.h
@@ -5,8 +5,17 @@ namespace TGA
 	class Camera
 	{
 	public:
-		Camera(void);
+		Camera(GLfloat xPos, GLfloat yPos);
 		~Camera(void);
+
+		void moveOver(GLfloat xVal, GLfloat yVal);
+		void setPosition(GLfloat xPos, GLfloat yPos);
+
+		GLfloat getX();
+		GLfloat	getY();
+
+	private:
+		GLfloat xPos, yPos;
 	};
 }
 
--- src/Camera.cpp
+++ src/Camera.cpp
@@ -2,13 +2,41 @@
 
 namespace TGA
 {
-	Camera::Camera(void)
+	Camera::Camera(GLfloat xPos, GLfloat yPos)
 	{
+		// Set this xPos to xPos
+		// Set this yPos to yPos
 	}
 
 
 	Camera::~Camera(void)
 	{
+	}
+
+	void Camera::moveOver(GLfloat xVal, GLfloat yVal)
+	{
+		// Update camera xPos (+=)
+
+		// Update camera yPos (-=)
+	}
+
+	void Camera::setPosition(GLfloat xPos, GLfloat yPos)
+	{
+		// Set xPos
+
+		// Set yPos
+	}
+
+	GLfloat Camera::getX()
+	{
+		// RETURN xPos
+		return xPos;
+	}
+
+	GLfloat Camera::getY()
+	{
+		// RETURN yPos
+		return yPos;
 	}
 }
 
