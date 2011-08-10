b57873f8ec92866e2fe3e1f2b34c2da453a4e14f
Tag Ashby <tgashby@calpoly.edu>
2011-08-10 15:04:35 -0700

    App coded, GraphicsManager pseudocoded

--- include/GraphicsManager.h
+++ include/GraphicsManager.h
@@ -1,12 +1,35 @@
 #pragma once
+#include "ProjIncludes.h"
 
 namespace TGA
 {
 	class GraphicsManager
 	{
 	public:
-		GraphicsManager(void);
-		~GraphicsManager(void);
+		GraphicsManager();
+		~GraphicsManager();
+
+		void init(int screenWidth = 1024, int screenHeight = 512, 
+		 std::string winTitle = "Sandstorms");
+
+		void shutDown();
+
+		void swapBuffers();
+
+		void resizeWindow(int screenWidth, int screenHeight);
+		void toggleFullScreen();
+
+	private:
+		void initGL();
+
+		int screenWidth;
+		int screenHeight;
+
+		bool isLoaded;
+		SDL_bool isFullScreen;
+
+		SDL_Window* window;
+		SDL_GLContext glCtx;
 	};
 }
 
--- src/GraphicsManager.cpp
+++ src/GraphicsManager.cpp
@@ -2,12 +2,100 @@
 
 namespace TGA
 {
-	GraphicsManager::GraphicsManager(void)
+	GraphicsManager::GraphicsManager()
 	{
+		// Set loaded to false
 	}
 	
 	
-	GraphicsManager::~GraphicsManager(void)
+	GraphicsManager::~GraphicsManager()
 	{
+		// Call shutDown()
+	}
+
+	void GraphicsManager::init(int screenWidth /*= 1024*/, int screenHeight /*= 512*/, 
+	 std::string winTitle /*= "Sandstorms"*/)
+	{
+		// Call SDL_INIT(SDL_EVERYTHING)
+
+		// Set the SDL GL Attributes
+		/*SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
+		SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
+		SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
+		SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 8);
+		SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 16);
+		SDL_GL_SetAttribute(SDL_GL_BUFFER_SIZE, 32);
+		SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);*/
+
+		// Create the window
+
+		// Create the GL context
+
+		// InitGL
+	}
+
+	void GraphicsManager::shutDown()
+	{
+		// IF it was ever loaded
+			// Delete the window
+			// Delete the gl context
+
+		// SDL_Quit()
+	}
+
+	void GraphicsManager::swapBuffers()
+	{
+		// IF it was loaded
+			// Call SDL_GL_SwapWindow(window)
+	}
+
+	void GraphicsManager::resizeWindow(int screenWidth, int screenHeight)
+	{
+		// IF it was loaded
+			// Destory the GL context
+			// Destroy the previous window
+
+			// Create a new window (window = SDL_CreateWindow.....)
+			// Create a new GL context
+
+			// Call initGL
+
+			// Reload all textures
+		// ENDIF
+	}
+
+	void GraphicsManager::toggleFullScreen()
+	{
+		//Call SDL_SetWindowFullscreen(window, isFullScreen)
+
+		// IF fullscreen
+
+			// Hide the cursor (SDL_ShowCursor(SDL_DISABLE))
+
+		// Else
+
+			// Unhide the cursor
+	}
+
+	void GraphicsManager::initGL()
+	{
+		glMatrixMode(GL_PROJECTION);
+
+		glLoadIdentity();
+
+		glOrtho(0.0, screenWidth, screenHeight, 0.0, -1, 1);
+
+		glDisable(GL_DEPTH_TEST);
+
+		glClearColor(1, 1, 1, 1);
+
+		glShadeModel(GL_SMOOTH);
+
+		glMatrixMode(GL_MODELVIEW);
+
+		glEnable(GL_BLEND);
+		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+		glLoadIdentity();
 	}
 }
